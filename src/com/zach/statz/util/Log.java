2f9fb9b85e86c0aa62a6fc28694b5063c2d4beb3
Zach Fiorito <zbfiorito@gmail.com>
2014-07-24 01:08:46 -0500

    Updated Logger

--- src/com/zach/statz/util/Log.java
+++ src/com/zach/statz/util/Log.java
@@ -5,21 +5,21 @@ public class Log {
 	private static final String COLOR_MC_CHAR = Character.toString((char) 167);
 
 	public static void info(String msg){
-			System.out.println(colorChat(msg));
+		System.out.println(colorChat(msg));
 	}
 	public static void config(String msg){
-			info(msg);
+		info(msg);
 	}
 	public static void warn(String msg){
-			System.err.println(colorChat(msg));
+		System.err.println(colorChat(msg));
 	}
 	public static void err(String msg){
-			System.err.println(colorChat(msg));
+		System.err.println(colorChat(msg));
 	}
 
-    public static String colorChat(String msg) {
-        return msg.replaceAll("&", COLOR_MC_CHAR);
-    }
+	public static String colorChat(String msg) {
+		return msg.replaceAll("&", COLOR_MC_CHAR);
+	}
 	public static void debug(String string) {
 		System.out.println(string);
 	}
