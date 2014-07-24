7a3e3157c253be49f29c274603566e938e7a84ab
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-07-24 00:23:59 -0500

    Fixed PlayerLoginEvent
    Added
    @EventHandler annotation

--- src/com/zach/statz/util/EventListener.java
+++ src/com/zach/statz/util/EventListener.java
@@ -1,11 +1,17 @@
 package com.zach.statz.util;
 
+import java.util.UUID;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 public class EventListener implements Listener {
 
-	public void onPlayerLogin(){
-		//MySQL.addStat();
-		
-	}
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent e) {
+        Player player = e.getPlayer();
+        UUID id = player.getUniqueId();
+        // MySQL.addStat(player);
+    }
 }
